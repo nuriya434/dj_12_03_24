21e3690729d8ebf8384f27bd00bef3acac090019
nuriya <nuriya434@mail.ru>
2024-03-03 14:08:48 +0600

    methods

--- src/post/models.py
+++ src/post/models.py
@@ -1,6 +1,8 @@
-# models.py
+import random
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=128)
-    photo = models.ImageField("..\photo.jpg", null=True)
+    title = models.CharField(max_length=128, null=True, default=f'post {int(random.random() * 100)}')
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    is_actual = models.BooleanField(blank=True, null=True)
